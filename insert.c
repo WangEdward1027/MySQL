41b8d1cdee0f548d46b6fbb9789481d1340a100b
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-06-11 21:56:16 +0800

    用C语言API向数据库插入数据

---
+++ insert.c
@@ -0,0 +1,33 @@
+//insert.c
+
+#include <func.h>
+#include <mysql/mysql.h>
+#include <stdlib.h>
+
+int main()
+{
+    //初始化MYSQL连接
+    MYSQL conn;
+    MYSQL* pconn = mysql_init(&conn);
+
+    //与MySQL服务器建立连接
+    pconn = mysql_real_connect(&conn, "localhost", "root", "1234", "cpp58", 0, NULL, 0);
+    if(pconn == NULL) {
+        printf("%s\n", mysql_error(&conn));
+        return EXIT_FAILURE;
+    }
+
+    //进行查询操作
+    const char * query = "INSERT INTO Student VALUES(6, 'Kris', '2005-01-01',90,100,80)";
+    int ret = mysql_query(pconn, query);
+    if(ret !=0) {
+        printf("(%d, %s)\n", mysql_errno(pconn), mysql_error(pconn));
+        return EXIT_FAILURE;
+    } else {
+        printf("QUERY OK, %ld row afftected.\n",
+               mysql_affected_rows(pconn));
+    }
+
+    mysql_close(pconn);
+    return 0;
+}
