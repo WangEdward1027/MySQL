143d67a655043a814318ce7ba7e0b38de5fc7b74
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-06-12 16:39:29 +0800

    预处理 SQL 语句: 使用MYSQL_STMT结构体插入数据，能防止SQL注入攻击

--- stmt_insert.c
+++ stmt_insert.c
@@ -1,5 +1,4 @@
-//预处理 SQL 语句: 使用MYSQL_STMT结构体插入数据，能防止SQL注入攻击
-              //还能模板化SQL语句，提高查询效率
+//预处理 SQL 语句: 使用MYSQL_STMT结构体插入数据，能防止SQL注入攻击, 还能模板化SQL语句，提高查询效率
 
 #include <func.h>
 #include <mysql/field_types.h>
@@ -12,11 +11,11 @@ int main()
     MYSQL conn;
     MYSQL * pconn = mysql_init(&conn);
     //连接MySQL服务器
-    const char * host = "localhost";   //IP地址
+    const char * host = "localhost";
     const char * user = "root";
     const char * passwd = "1234";
     const char * db = "cpp58";
-    pconn = mysql_real_connect(pconn, host, user, passwd, db, 0, NULL, 0);
+    pconn = mysql_real_connect(pconn, host, user, passwd, db, 3306, NULL, 0);
     if(pconn == NULL) {
         printf("error:%s\n", mysql_error(&conn));
         return EXIT_FAILURE;
@@ -34,8 +33,7 @@ int main()
     const char * sql = "INSERT INTO t_user VALUES(?,?,?)";
     int ret = mysql_stmt_prepare(stmt, sql, strlen(sql));
     if(ret) {
-        printf("(%d, %s\n)", mysql_stmt_errno(stmt),
-               mysql_stmt_error(stmt));
+        printf("(%d, %s\n)", mysql_stmt_errno(stmt), mysql_stmt_error(stmt));
         mysql_close(pconn);
         return EXIT_FAILURE;
     }
@@ -43,8 +41,7 @@ int main()
     //获取模板化的sql中占位符的个数
     int count = mysql_stmt_param_count(stmt);
     if(count != 3) {
-        printf("(%d, %s\n)", mysql_stmt_errno(stmt),
-               mysql_stmt_error(stmt));
+        printf("(%d, %s\n)", mysql_stmt_errno(stmt), mysql_stmt_error(stmt));
         mysql_close(pconn);
         return EXIT_FAILURE;
     }
@@ -75,6 +72,7 @@ int main()
     bind[2].buffer = password;
     bind[2].is_null = 0;
     bind[2].length = &passwd_len;
+
     //执行绑定操作
     ret = mysql_stmt_bind_param(stmt, bind);
     if(ret) {
@@ -85,39 +83,34 @@ int main()
     }
 
     //第一次传递参数
-    id = 3;
-    strcpy(username, "唐三");
-    name_len = strlen("唐三");
-    strcpy(password, "8888");
-    passwd_len = strlen("8888");
+    id = 5;
+    strcpy(username, "Sam");
+    name_len = strlen("Sam");
+    strcpy(password, "20240701");
+    passwd_len = strlen("20240701");
     //第一次执行EXECUTE语句
     ret = mysql_stmt_execute(stmt);
     if(ret) {
-        printf("(%d, %s\n)", mysql_stmt_errno(stmt),
-               mysql_stmt_error(stmt));
+        printf("(%d, %s\n)", mysql_stmt_errno(stmt), mysql_stmt_error(stmt));
         mysql_close(pconn);
         return EXIT_FAILURE;
     }
-
-    printf("QUERY OK, %ld row affected.\n",
-           mysql_stmt_affected_rows(stmt));
+    printf("QUERY OK, %ld row affected.\n", mysql_stmt_affected_rows(stmt));
+    
     //第二次传递参数
-    id = 4;
-    strcpy(username, "比比东");
-    name_len = strlen("比比东");
-    strcpy(password, "99999");
-    passwd_len = strlen("99999");
+    id = 6;
+    strcpy(username, "Windy");
+    name_len = strlen("Windy");
+    strcpy(password, "20240601");
+    passwd_len = strlen("20240601");
     //第二次执行EXECUTE语句
     ret = mysql_stmt_execute(stmt);
     if(ret) {
-        printf("(%d, %s\n)", mysql_stmt_errno(stmt),
-               mysql_stmt_error(stmt));
+        printf("(%d, %s\n)", mysql_stmt_errno(stmt), mysql_stmt_error(stmt));
         mysql_close(pconn);
         return EXIT_FAILURE;
     }
-
-    printf("QUERY OK, %ld row affected.\n",
-           mysql_stmt_affected_rows(stmt));
+    printf("QUERY OK, %ld row affected.\n", mysql_stmt_affected_rows(stmt));
 
     mysql_stmt_close(stmt);
     mysql_close(pconn);
